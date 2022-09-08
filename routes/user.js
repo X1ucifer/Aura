89879f89bd15312e01f304fce8d80a225f2c59cb
Akhil Jinu <portforwarding03@gmail.com>
2022-09-08 22:43:57 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const { signup,login, current_user } = require('../Controllers/user_controller/auth');
+const { signup,login, current_user } = require('../controllers/user_controller/auth.js');
 
 
 router.post('/signup',signup);
