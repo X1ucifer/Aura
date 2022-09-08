c907eb5f478b93af0318a56f5c99bbbbfababc6c
Akhil Jinu <portforwarding03@gmail.com>
2022-09-09 00:38:50 +0530

    get

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const { signup,login, current_user } = require('../controllers/user_controller/a
 
 
 router.post('/signup',signup);
+
 router.post('/login',login);
 router.get('/get',current_user);
 
