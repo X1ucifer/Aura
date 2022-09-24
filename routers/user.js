e625b5f044fd673c3aa022c3d1a3d8ab60a38100
Akhil Jinu <portforwarding03@gmail.com>
2022-09-25 00:49:49 +0530

    Revert "Login"
    This reverts commit a3eeee5710fb30f64548958cd03e40e02f01e329.

--- routers/user.js
+++
@@ -1,5 +0,0 @@
-const Router = require('express').Router();
-const {signup, login} = require('../controllers/auth.js');
-Router.post('/signup',signup);
-Router.post('/login',login);
-module.exports = Router;
